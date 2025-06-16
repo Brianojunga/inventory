6d38f7afdd049c5d9093ee40cb394b672ba0769c
Brianojunga <brianojunga3@gmail.com>
2025-06-16 19:52:53 +0300

    Add SSL acceptance of any db

--- Backend/db/pool.js
+++ Backend/db/pool.js
@@ -1,9 +1,10 @@
 require("dotenv").config();
 const { Pool } = require("pg");
 
+
+const isProduction = process.env.NODE_ENV === "production";
+
 module.exports = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false,
-  },
+  ssl : isProduction ? { rejectUnauthorized : false} : false,
 });
